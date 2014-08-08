3b97d3985d2e7ba4c6e033c71239bd6170d5884e
Nico Grashoff <ngrash@gmail.com>
2014-08-08 17:07:17 +0200

    Add TemplateCompiler

---
+++ CoCon.Templates.Tests/TemplateCompilerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoCon.Templates.Tests
+{
+    [TestClass]
+    public class TemplateCompilerTests
+    {
+        [TestMethod]
+        public void CompilesAndReturnsRuntimeGeneratedTemplateClass()
+        {
+            const string TemplateCode = @"class RuntimeGeneratedTemplateClass { }";
+
+            var compiler = new TemplateCompiler();
+            Type result = compiler.CompileTemplate(TemplateCode);
+
+            Assert.IsNotNull(result, "Result type is null");
+        }
+    }
+}
---
+++ CoCon.Templates/TemplateCompiler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+using Microsoft.CSharp;
+
+namespace CoCon.Templates
+{
+    public class TemplateCompiler
+    {
+        public Type CompileTemplate(string template)
+        {
+            var codeProvider = new CSharpCodeProvider();
+            var options = new CompilerParameters();
+            options.ReferencedAssemblies.Add(Assembly.GetExecutingAssembly().Location);
+            CompilerResults result = codeProvider.CompileAssemblyFromSource(options, template);
+
+            Type templateType = result.CompiledAssembly.GetType("RuntimeGeneratedTemplateClass");
+
+            return templateType;
+        }
+    }
+}
