3a92ed86f99b638d5e1c33d8aef0f22092ec2e5d
Nico Grashoff <ngrash@gmail.com>
2014-08-08 17:00:08 +0200

    Add TemplateClassFactory

---
+++ CoCon.Templates.Tests/TemplateClassFactoryTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+using CoCon.Templates.Parser;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoCon.Templates.Tests
+{
+    [TestClass]
+    public class TemplateClassFactoryTests
+    {
+        public const string TestPlainText = "fizzbuzz";
+        public const string TestCodeBlock = "foo();";
+
+        [TestMethod]
+        public void CreatesEmptyTemplate()
+        {
+            var factory = new TemplateClassFactory();
+            string templateClass = factory.CreateTemplateClass(new List<TemplateSegment>());
+
+            Assert.IsNotNull(templateClass);
+            Assert.AreNotEqual(string.Empty, templateClass);
+
+            // Some sanity checks
+            Assert.IsFalse(templateClass.Contains(TestPlainText), "Empty template class contains test plain text");
+            Assert.IsFalse(templateClass.Contains(TestCodeBlock), "Empty template class contains test code block");
+        }
+
+        [TestMethod]
+        public void InsertsPlainText()
+        {
+            var factory = new TemplateClassFactory();
+            var segments = new List<TemplateSegment>
+                {
+                    new TemplateSegment(TemplateSegmentType.PlainText, TestPlainText)
+                };
+
+            string templateClass = factory.CreateTemplateClass(segments);
+
+            Assert.IsTrue(templateClass.Contains(TestPlainText));
+        }
+
+        [TestMethod]
+        public void InsertsCodeBlock()
+        {
+            var factory = new TemplateClassFactory();
+            var segments = new List<TemplateSegment>
+                {
+                    new TemplateSegment(TemplateSegmentType.CodeBlock, TestCodeBlock)
+                };
+
+            string templateClass = factory.CreateTemplateClass(segments);
+
+            Assert.IsTrue(templateClass.Contains(TestCodeBlock));
+        }
+
+        [TestMethod]
+        public void InsertsInRightOrder()
+        {
+            var factory = new TemplateClassFactory();
+            var segments = new List<TemplateSegment>
+                {
+                    new TemplateSegment(TemplateSegmentType.PlainText, TestPlainText),
+                    new TemplateSegment(TemplateSegmentType.CodeBlock, TestCodeBlock)
+                };
+
+            string templateClass = factory.CreateTemplateClass(segments);
+
+            int indexOfTestPlainText = templateClass.IndexOf(TestPlainText, System.StringComparison.Ordinal);
+            int indexOfTestCodeBlock = templateClass.IndexOf(TestCodeBlock, System.StringComparison.Ordinal);
+
+            Assert.IsTrue(indexOfTestPlainText < indexOfTestCodeBlock, "Plain text comes before code block");
+        }
+    }
+}
---
+++ CoCon.Templates/TemplateClassFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CoCon.Templates.Parser;
+
+namespace CoCon.Templates
+{
+    public class TemplateClassFactory
+    {
+        public string CreateTemplateClass(IReadOnlyList<TemplateSegment> segments)
+        {
+            var templateBuilder = new StringBuilder();
+
+            templateBuilder.AppendLine("public class RuntimeGeneratedTemplateClass : global::CoCon.Templates.TemplateBase {");
+            templateBuilder.AppendLine("\tprotected override void ProcessInternal() {");
+
+            foreach (TemplateSegment segment in segments)
+            {
+                switch (segment.Type)
+                {
+                    case TemplateSegmentType.PlainText:
+                        string writeCall = string.Format("Write(\"{0}\")", segment.Content);
+                        templateBuilder.Append(writeCall);
+                        break;
+                    case TemplateSegmentType.CodeBlock:
+                        templateBuilder.Append(segment.Content);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            templateBuilder.AppendLine("\t}");
+            templateBuilder.AppendLine("}");
+
+            return templateBuilder.ToString();
+        }
+    }
+}
