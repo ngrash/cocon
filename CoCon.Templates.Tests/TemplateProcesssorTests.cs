31dd7bfbc3318770a00f0cc66de22463497115da
Nico Grashoff <ngrash@gmail.com>
2014-08-08 17:10:36 +0200

    Add TemplateProcessor

---
+++ CoCon.Templates.Tests/TemplateProcesssorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoCon.Templates.Tests
+{
+    [TestClass]
+    public class TemplateProcesssorTests
+    {
+        [TestMethod]
+        public void ReturnsStringResultOfProcessMethod()
+        {
+            var processor = new TemplateProcessor();
+            string result = processor.ProcessTemplate(typeof(TestClass));
+
+            Assert.AreEqual("success", result);
+        }
+
+        private class TestClass
+        {
+            public string Process()
+            {
+                return "success";
+            }
+        }
+    }
+}
---
+++ CoCon.Templates/TemplateProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace CoCon.Templates
+{
+    public class TemplateProcessor
+    {
+        public string ProcessTemplate(Type templateType)
+        {
+            object instance = Activator.CreateInstance(templateType);
+
+            MethodInfo processMethod = templateType.GetMethod("Process");
+            var result = (string)processMethod.Invoke(instance, null);
+
+            return result;
+        }
+    }
+}
