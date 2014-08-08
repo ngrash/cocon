f0c3a2f8bdbae408ff014927be2158f8cafe44a8
Nico Grashoff <ngrash@gmail.com>
2014-08-08 16:33:32 +0200

    Add TemplateBase

---
+++ CoCon.Templates.Tests/TemplateBaseTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoCon.Templates.Tests
+{
+    [TestClass]
+    public class TemplateBaseTests
+    {
+        [TestMethod]
+        public void ProcessCallsProcessInternal()
+        {
+            var template = new TestTemplate();
+            template.Process();
+
+            Assert.IsTrue(template.ProcessInternalCalled);
+        }
+
+        [TestMethod]
+        public void ProcessReturnsEmptyString()
+        {
+            var template = new TestTemplate();
+            string result = template.Process();
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void WriteWritesToContent()
+        {
+            var template = new TestTemplate();
+            template.Write("foo");
+            string result = template.Process();
+
+            Assert.AreEqual("foo", result);
+        }
+
+        private class TestTemplate : TemplateBase
+        {
+            public bool ProcessInternalCalled { get; private set; }
+
+            public new void Write(string content)
+            {
+                base.Write(content);
+            }
+
+            protected override void ProcessInternal()
+            {
+                ProcessInternalCalled = true;
+            }
+        }
+    }
+}
---
+++ CoCon.Templates/TemplateBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace CoCon.Templates
+{
+    public abstract class TemplateBase
+    {
+        private readonly StringBuilder _contentBuilder = new StringBuilder();
+
+        public string Process()
+        {
+            ProcessInternal();
+
+            return _contentBuilder.ToString();
+        }
+
+        protected abstract void ProcessInternal();
+
+        protected void Write(string content)
+        {
+            _contentBuilder.Append(content);
+        }
+    }
+}
